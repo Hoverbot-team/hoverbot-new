eb4e1c6f79ad74f40a718b3017dd113122202fd1
Michasssada <michaslate@gmail.com>
2024-12-04 15:25:18 +0100

    mapVal added

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 
 using namespace std;
+float mapValue(float x, float in_min, float in_max, float out_min, float out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 int main(){
     MPU6050 gyro;
     while(1){
