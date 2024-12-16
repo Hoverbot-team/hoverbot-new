83bb57e7601544d6c46644900dcc2d58f72cce61
Michasssada <michaslate@gmail.com>
2024-12-16 20:57:30 +0100

    added function for connection checking

--- raspberry/src/main.cpp
+++ raspberry/src/main.cpp
@@ -2,14 +2,24 @@
 #include <iostream>
 #include "uart.hpp"
 using namespace std;
+//global variables
+UART uart(115200);
+MPU6050 gyro;
+
 float mapValue(float x, float in_min, float in_max, float out_min, float out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+void checkArduinoConnection(){
+    string in = uart.read();
+    while(in != "rr"){
+        in = uart.read();
+    }
+    uart.write("ar");
 }
 int main(){
 
 
-    UART uart(115200);
-    MPU6050 gyro;
+
     while(1){
     }
     
