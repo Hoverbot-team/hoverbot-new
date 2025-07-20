4b6ebec4adda7ded764dc8b44d6a6da1b9bcd21d
Michasssada <michaslate@gmail.com@gmail.com>
2025-07-20 22:51:20 +0100

    work on magnetometer

--- include/sensors/rotation/magnetometer.hpp
+++ include/sensors/rotation/magnetometer.hpp
@@ -7,9 +7,12 @@
 class HMC5883L {
 public:
     HMC5883L();
-
+    int16_t readRawX();
+    int16_t readRawY();
+    int16_t readRawZ();
 private:
     int fd; // File descriptor for I2C communication
     int readWord2C(int addr);
     float convertToDegrees(int16_t raw_value);
+    int16_t readI2c(int addr);
 };
